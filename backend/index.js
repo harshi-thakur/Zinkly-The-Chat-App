7ce1eee33bf4d60f2a59c40f0567aa58c0907636
harshi-thakur <abcharshitthakur@gmail.com>
2025-07-15 18:13:22 +0530

    fixed cors

--- backend/index.js
+++ backend/index.js
@@ -14,14 +14,10 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-
-const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-};
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
-
+app.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173", 
+  credentials: true,              
+}));
 app.use(express.json());
 app.use(cookieParser());
 
