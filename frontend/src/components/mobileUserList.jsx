d003c696b21191bbbc262fd63012071d13db4e1d
harshi-thakur <abcharshitthakur@gmail.com>
2025-07-15 17:13:26 +0530

    minor changes to mobile user list component

--- frontend/src/components/mobileUserList.jsx
+++ frontend/src/components/mobileUserList.jsx
@@ -14,9 +14,10 @@ export function MobileUserList() {
             <div className="w-3 h-3 bg-green-500 rounded-full"></div>
             <h3 className="font-semibold text-gray-900">Online ({onlineMembers.length})</h3>
           </div>
-          <div className="space-y-3">
+          <ScrollArea className="max-h-52 " >
+      
             {onlineMembers.map((user) => (
-              <div key={user._id} className="flex items-center gap-3">
+              <div key={user._id} className="flex items-center gap-3 py-2">
                 <div className="relative">
                   <Avatar className="w-10 h-10">
                     <AvatarImage src={user.profilePic || "/placeholder.svg"} />
@@ -30,19 +31,23 @@ export function MobileUserList() {
                   <div className="absolute -bottom-1 -right-1 w-4 h-4 bg-green-500 rounded-full border-2 border-white"></div>
                 </div>
                 <div className="flex-1 min-w-0">
-                  <p className="font-medium text-gray-900">{user.fullname}</p>
+                  <div className="flex items-center  gap-5">
+                  <p className="font-medium text-gray-900 max-w-1/2 truncate">{user.fullname}</p>
+                  <p className="font-medium text-gray-400 max-w-1/2">{"@"+user.username}</p>
+                  </div>
                   <p className="text-sm text-green-600">Online</p>
                 </div>
               </div>
             ))}
-          </div>
+      
+            </ScrollArea>
         </div>
       )}
 
       {/* All members */}
       <div className="p-4">
         <h3 className="font-semibold text-gray-900 mb-3">All members ({activeRoomMembers.length})</h3>
-        <ScrollArea className="space-y-3">
+        <ScrollArea className="max-h-52 " >
           {activeRoomMembers.map((user) => (
             <div key={user._id} className="flex items-center gap-3 py-2">
               <div className="relative">
@@ -60,7 +65,10 @@ export function MobileUserList() {
                 )}
               </div>
               <div className="flex-1 min-w-0">
-                <p className="font-medium text-gray-900">{user.fullname}</p>
+                <div className="flex items-center  gap-5">
+                  <p className="font-medium text-gray-900 max-w-1/2 truncate">{user.fullname}</p>
+                  <p className="font-medium text-gray-400 max-w-1/2">{"@"+user.username}</p>
+                  </div>
                 <p className="text-sm text-gray-600">{isUserOnline(user._id) ? "Online" : "Offline"}</p>
               </div>
             </div>
